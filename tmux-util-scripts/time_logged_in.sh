e788b4727831034503b5cbee7fddd30612bc287a
Elias Nyström <elnystro@cisco.com>
2017-01-13 09:38:51 +0100

    Made the minute calculation look better

--- tmux-util-scripts/time_logged_in.sh
+++ tmux-util-scripts/time_logged_in.sh
@@ -5,7 +5,7 @@ now=`date +%s`
 let diff="$now - $logindate"
 # echo $diff
 let hrs="$diff / 3600"
-let mins="($diff / 60) - $hrs * 60"
+let mins="($diff % 3600) / 60"
 let secs="$diff % 60"
 if [ "$1" = "-s" ]; then
     printf '%d:%02d:%02d\n' "$hrs" "$mins" "$secs"
