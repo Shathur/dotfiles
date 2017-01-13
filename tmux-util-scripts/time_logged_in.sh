f412ff8f7f521dd3957b938171c72c1f952ce58c
Elias Nyström <elnystro@cisco.com>
2017-01-13 09:36:52 +0100

    Fixed minute calculation in time logged in script

--- tmux-util-scripts/time_logged_in.sh
+++ tmux-util-scripts/time_logged_in.sh
@@ -3,8 +3,9 @@ logintime=`w -h | cut -d " " -f 22`
 logindate=`date -d "$logintime" +%s`
 now=`date +%s`
 let diff="$now - $logindate"
+# echo $diff
 let hrs="$diff / 3600"
-let mins="$diff / 60"
+let mins="($diff / 60) - $hrs * 60"
 let secs="$diff % 60"
 if [ "$1" = "-s" ]; then
     printf '%d:%02d:%02d\n' "$hrs" "$mins" "$secs"
