0dbbfd0d4a28a703990aa76b3cf421783e9b2797
Elias Nyström <elnystro@cisco.com>
2017-01-13 08:21:07 +0100

    Added seconds switch and leading zeroes to time_logged_in

--- tmux-util-scripts/time_logged_in.sh
+++ tmux-util-scripts/time_logged_in.sh
@@ -6,6 +6,8 @@ let diff="$now - $logindate"
 let hrs="$diff / 3600"
 let mins="$diff / 60"
 let secs="$diff % 60"
-echo "$hrs:$mins:$secs"
-# echo with printf to get padding (always to digits each)
-# also, add switch to turn off seconds
+if [ "$1" = "-s" ]; then
+    printf '%d:%02d:%02d\n' "$hrs" "$mins" "$secs"
+else
+    printf '%d:%02d\n' "$hrs" "$mins"
+fi
